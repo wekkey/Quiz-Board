b48ae65b5a5735ed57aedade7d45b408ac13b9b1
wekkey <stellzwetkke@gmail.com>
2019-04-01 14:51:17 +0300

    add the function check to the javascript section to identify the specified questions.

--- js/scripts.js
+++ js/scripts.js
@@ -6,24 +6,4 @@ function check() {
     var questionFive = document.Quiz.questionFive.value;
     var right =0;
 
-    if (questionOne == "correct") {
-        right = right + 20;
-    };
-    if (questionTwo == "correct"){
-        right= right+20;
-    };
-    if (questionThree == "correct"){
-        right= right+ 20;
-    };
-    if (questionFour == "correct"){
-        right= right+ 20;
-    };
-    if (questionFive == "correct"){
-        right= right+20;
-    };
-
-
-
-    document.getElementById("fifth-p").style.visibility="visible";
-    document.getElementById("correctNumber").innerHTML="HERE IS YOUR SCORE: " + right + "%";
-}
+    
